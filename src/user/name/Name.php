c748f4563f0a255059bbf6f16da72130a9884e63
Iuoras Razvan <iuorasrazvan2003@yahoo.com>
2018-03-22 10:49:14 +0200

    v1.0.5

--- src/user/name/Name.php
+++ src/user/name/Name.php
@@ -12,7 +12,8 @@
 			echo 'version 1.0.0 - without alias';
 			echo 'version 1.0.1 -with alias';
 			echo "version 1.0.2 stable version";
-			echo "version 1.0.3 beta"
+			echo "version 1.0.3 beta";
+			echo "version 1.0.4 rc1";
 			
 		}
 		
